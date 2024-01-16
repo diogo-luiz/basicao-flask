5688f166d51c41abd2fdc091700bd3ca9290186e
Diogo <profissional.diogoribeiro@gmail.com>
2024-01-16 11:08:01 -0300

    updating python files

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix="/admin")
 
-@app.route("/home")
 @app.route("/")
-def home():
-    return render_template("home.html")
+def test():
+    return '<h1>Teste</h1>'#render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
