cbe3c5b1d6312dbbd2461b69b5f758aaae6d2743
Diogo <profissional.diogoribeiro@gmail.com>
2024-01-24 00:52:49 -0300

    Updating files

--- Flask REST API/main.py
+++ Flask REST API/main.py
@@ -1,22 +1,36 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-    "dio": {"age": 23, "gender": "male"},
-    "bill": {"age": 70, "gender": "male"}}
+# names = {
+#     "dio": {"age": 23, "gender": "male"},
+#     "bill": {"age": 70, "gender": "male"}}
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+
+video_put_args = reqparse.RequestParser() # parse the request automatically
+video_put_args.add_argument("name", type=str, help="Name of the video", required= True)
+video_put_args.add_argument("view", type=int, help="View of video", required= True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required= True)
+
+videos = {}
+
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
     # def post(self):
     #     return{"data":"Posted"}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+    def put(self, video_id):
+        args = video_put_args.parse_args()
+        return {video_id: args}
+        
+
+#api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 
 if __name__ == "__main__":
--- Flask REST API/test.py
+++ Flask REST API/test.py
@@ -2,5 +2,5 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/bill")
+response = requests.put(BASE + "video/1", {"name":"Video","likes": 10})
 print(response.json())
