b454b2bcbde7275357e58dfcc101b0a5d846ac7f
Diogo <profissional.diogoribeiro@gmail.com>
2024-01-23 23:52:21 -0300

    Updating .py files

--- Flask REST API/main.py
+++ Flask REST API/main.py
@@ -4,12 +4,20 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data":"hello world"}
+names = {
+    "dio": {"age": 23, "gender": "male"},
+    "bill": {"age": 70, "gender": "male"}}
 
-api.add_resource(HelloWorld, "/helloworld")
-        
+
+class HelloWorld(Resource):
+    def get(self, name):
+        return names[name]
+
+    # def post(self):
+    #     return{"data":"Posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
