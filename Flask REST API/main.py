8203d18f16f4bc9c5fed570ef9d1999a4a099c97
Diogo <profissional.diogoribeiro@gmail.com>
2024-01-24 21:54:02 -0300

    Updating

--- Flask REST API/main.py
+++ Flask REST API/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,14 +11,19 @@ api = Api(app)
 
 
 video_put_args = reqparse.RequestParser() # parse the request automatically
-video_put_args.add_argument("name", type=str, help="Name of the video", required= True)
-video_put_args.add_argument("view", type=int, help="View of video", required= True)
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required= True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required= True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video", required= True)
 
 videos = {}
 
+def abort_id(video_id):
+    if video_id not in videos:
+        abort(404, message = "Video id is not valid...")
+
 class Video(Resource):
     def get(self, video_id):
+        abort_id(video_id)
         return videos[video_id]
 
     # def post(self):
@@ -26,7 +31,8 @@ class Video(Resource):
 
     def put(self, video_id):
         args = video_put_args.parse_args()
-        return {video_id: args}
+        videos[video_id] = args
+        return {video_id: args}, 201
         
 
 #api.add_resource(HelloWorld, "/helloworld/<string:name>")
--- Flask REST API/test.py
+++ Flask REST API/test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"name":"Video","likes": 10})
+response = requests.put(BASE + "video/1", {"likes": 10, "name": "Dio", "views": 100000})
+print(response.json())
+input()
+response = requests.get(BASE + "video/4")
 print(response.json())
